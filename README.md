8bf925a50ac5cc389212bb51423e628a21ca5e83
Tom Pusateri <pusateri@bangj.com>
2018-03-18 13:58:47 +0000

    Add axios and send dns request in POST.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Web DNS over HTTP (DOH) test tool
     npm install --save-dev webpack webpack-cli webpack-dev-server
     npm install --save-dev style-loader css-loader url-loader
     npm install --save-dev bootstrap jquery popper.js
+    npm install --save-dev axios
 
 ## Run
     npm start
--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import packet from "dns-packet";
+import axios from 'axios';
 import "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function send(qname, rrtype, url) {
+  const buf = packet.encode({
+    type: 'query',
+    id: getRandomInt(1, 65534),
+    flags: packet.RECURSION_DESIRED,
+    questions: [{
+      type: rrtype,
+      name: qname
+    }]
+  });
+
+  axios.request({
+    url: url,
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/dns-udpwireformat',
+    },
+    data: buf
+  })
+  .then(function (response) {
+    console.log(packet.decode(response));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
 window.onload = function() {
-  document.getElementById("query").addEventListener(
-    "submit",
-    console.log("submit")
-  );
+  var submit = document.getElementById("query");
+  submit.addEventListener("click", function(event) {
+    event.preventDefault();
+    const qname = document.getElementById("qname").value;
+    const rrtype = document.getElementById("rrtype").value;
+    const https_url = document.getElementById("https_url").value;
+    send(qname, rrtype, https_url);
+  }, false);
 };
